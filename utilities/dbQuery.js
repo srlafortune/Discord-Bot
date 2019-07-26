d887be0eee57f1e3d36380257f230269834b55f2
Stephen Lafortune <slafor24@gmail.com>
2019-07-25 22:38:35 -0400

    digging and bug fixes

---
+++ utilities/dbQuery.js
@@ -0,0 +1,17 @@
+module.exports = function dbGet(params, dbClient) {
+    return new Promise((resolve, reject) => {
+        try {
+            dbClient.query(params, (err, data) => {
+                if (err) {
+                    console.error(err)
+                    reject(err)
+                } else {
+                    resolve(data)
+                }
+            })
+        } catch (err) {
+            console.error(err)
+            reject(err)
+        }
+    })
+}
